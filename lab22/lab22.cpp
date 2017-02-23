f7d409fc607ba71db80ecd47d45cb19562fcc27c
Enrique Chavez <echavez016@student.butte.edu>
2017-02-23 22:37:44 +0000

    Lab 2.2 submission

---
+++ lab22/lab22.cpp
@@ -0,0 +1,31 @@
+/*
+ Enrique Chavez 
+ CSCI 20 
+ Feb 23, 2017
+ 2.2 Lab - Writing Functions
+ 
+ this program generates a random number(Using a function) between 1 & 100 then outputs it 
+ 
+*/
+#include <iostream>
+#include <cstdlib> //needed to make random # 
+#include <ctime>  //needed to make random # 
+using namespace std;
+ 
+ 
+ void RandomNumber (){
+     
+       cout <<  rand() % 10 + 1 << "."<< endl;  
+ }
+ 
+ int main() {
+    
+   
+    srand(time(NULL));
+   
+   cout<<"Your Random Number is ";
+     RandomNumber();  
+   
+
+   return 0;  
+ };
