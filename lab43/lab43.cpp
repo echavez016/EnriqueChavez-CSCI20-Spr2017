c9f329ccafb994d64821e20b139c28fbab28c2a0
Enrique Chavez <echavez016@student.butte.edu>
2017-04-20 23:14:36 +0000

    lab 4.4 submission

---
+++ lab43/lab43.cpp
@@ -0,0 +1,45 @@
+/*
+ Enrique Chavez 
+ CSCI 20 
+ April 18, 2017
+4.3 Arrays of Objects
+
+Problem Statement
+This program mimics an online shopping cart.  
+You are the proprieter of an online shop and have to keep track of inventory, price and the user’s shopping cart. 
+Create a class to hold the object name, price, and current inventory.  
+Output all of the object names for your user to choose what they want.  
+As the user purchases an item, you should remove that item from inventory (subtract 1) and add the price to a total. 
+When the user is done adding items to their cart, you should output a statement of what they have ordered and the total cost of their purchases.  
+Do not allow you inventory to go below 0.
+
+Requirements
+Classes with constructor, mutators and accessors, input/output in the main.
+Array of objects (Shopping Cart)
+
+ 
+*/
+#include <iostream>
+#include <string> 
+using namespace std;
+
+class ShoppingCart{
+    public: 
+        objectName
+        price
+        currentInventory
+    
+    private:
+ 
+};
+
+
+    int main () {
+        
+  
+
+    
+    return 0; 
+    }
+
+
---
+++ testing/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include<cctype>
+#include<cstring>
+using namespace std;
+  
+  int main () {
+
+string myString = "my test string";
+myString[14] = '!';
+cout<<myString<<endl;
+    return 0; 
+    }
